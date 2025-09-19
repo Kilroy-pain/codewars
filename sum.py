e02d0b19ca23d906ae24946a9536ecd47aec0ea4
Bornventure Odhiambo <bornodhimbo@gmail.com>
2025-09-19 03:25:31 +0300

    Update sum.py

--- sum.py
+++ sum.py
@@ -1,21 +1,15 @@
 def sum_two_smallest_numbers(numbers):  
-    smallest_number = numbers[0]
-    second_smallest_number = numbers[1]
-    if numbers[0] < numbers[1]:
-        smallest_number = numbers[0]
-        second_smallest_number = numbers[1]
-        
-    else:
-        smallest_number = numbers[1]
-        second_smallest_number = numbers[0]   
-        
-    for number in numbers[2:-1]:
-        if number < smallest_number:
-            second_smallest_nummber = smallest_number
-            smallest_number = number
+    smallest = numbers[0]
+    for number in numbers:
+        if number < smallest:
+            smallest = number
             
-        elif number > smallest_number and number < second_smallest_number:
-            second_smallest_number = number
+    numbers.remove(smallest) 
+    
+    second_smallest = numbers[0]
+    for number in numbers:
+        if number < second_smallest:
+            second_smallest = number
         
         
-    return smallest_number + second_smallest_number
+    return smallest + second_smallest
